e2c4ef37c165f7e9c7bdabbcfb89c41cc8cdcbe4
Chlumsky-Ceros <viktor.chlumsky@ceros.com>
2023-03-07 23:20:07 +0100

    Fix macro redefinition warnings

--- tools/render-graph-inspector/imgui/ode_imgui_compatibility_impl_opengl3.cpp
+++ tools/render-graph-inspector/imgui/ode_imgui_compatibility_impl_opengl3.cpp
@@ -6,6 +6,15 @@
 // Disable vertex array functions if it's not enabled in ODE.
 //   ImGui uses their IMGUI_IMPL_OPENGL_USE_VERTEX_ARRAY define, however this is set within the imgui_impl_opengl3.cpp.
 #ifndef ODE_GL_ENABLE_VERTEX_ARRAYS
+#ifdef glBindVertexArray
+#undef glBindVertexArray
+#endif
+#ifdef glGenVertexArrays
+#undef glGenVertexArrays
+#endif
+#ifdef glDeleteVertexArrays
+#undef glDeleteVertexArrays
+#endif
 #define glBindVertexArray
 #define glGenVertexArrays
 #define glDeleteVertexArrays
