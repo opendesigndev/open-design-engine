e286bb16cb384e431288cd9d6fec7611ea795cf7
Isabella Skorepova <isabella@skorepova.info>
2023-03-14 11:39:59 +0100

    Fix git calling inside build

--- dockerfiles/wasm-build/build.sh
+++ dockerfiles/wasm-build/build.sh
@@ -4,5 +4,6 @@ set -ex
 if [ -d "${0%/*}" ]; then cd "${0%/*}" ; fi # go to directory containing this script
 cd ../.. # go to repo root
 
+git config --global --add safe.directory `pwd`
 cmake --preset wasm-rel
 cmake --build --preset wasm-rel
