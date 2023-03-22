f70ccaa1529f658aef329f816b0a30a2092aae36
Chlumsky-Ceros <viktor.chlumsky@ceros.com>
2023-03-22 09:07:46 +0100

    Fetch vcpkg commit based on manifest

--- version.cmake
+++ version.cmake
@@ -9,6 +9,9 @@ string(REGEX REPLACE "^([0-9]*)\\.([0-9]*)\\.([0-9]*)" "\\1" ODE_VERSION_MAJOR $
 string(REGEX REPLACE "^([0-9]*)\\.([0-9]*)\\.([0-9]*)" "\\2" ODE_VERSION_MINOR ${ODE_VERSION})
 string(REGEX REPLACE "^([0-9]*)\\.([0-9]*)\\.([0-9]*)" "\\3" ODE_VERSION_REVISION ${ODE_VERSION})
 
+string(REGEX MATCH "\"builtin-baseline\"[ \t\n\r]*:[ \t\n\r]*\"[^\"]*\"" ODE_TMP_BUILTIN_BASELINE_PAIR ${ODE_VCPKG_JSON})
+string(REGEX REPLACE "\"builtin-baseline\"[ \t\n\r]*:[ \t\n\r]*\"([^\"]*)\"" "\\1" ODE_VCPKG_BUILTIN_BASELINE ${ODE_TMP_BUILTIN_BASELINE_PAIR})
+
 if(NOT DEFINED ODE_BUILD_COMMIT_SHA)
     execute_process(COMMAND git describe --tags --always WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}" OUTPUT_VARIABLE ODE_BUILD_COMMIT_SHA OUTPUT_STRIP_TRAILING_WHITESPACE)
 endif()
@@ -27,4 +30,5 @@ endif()
 string(TIMESTAMP ODE_BUILD_DATE "%Y-%m-%d")
 
 unset(ODE_TMP_VERSION_PAIR)
+unset(ODE_TMP_BUILTIN_BASELINE_PAIR)
 unset(ODE_VCPKG_JSON)
